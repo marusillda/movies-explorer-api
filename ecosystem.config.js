78476b3b2a1478bd618102cc1c519790dd804921
Maria Dobrorodnova <maria.dobrorodnova@gmail.com>
2023-07-01 23:31:11 +0300

    feat: добавить pm2 deploy

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+  apps: [{
+    name: 'movies',
+    script: 'app.js',
+  }],
+  deploy: {
+    production: {
+      user: 'maria',
+      host: ['62.84.123.152'],
+      ref: 'origin/level-1',
+      repo: 'git@github.com:marusillda/movies-explorer-api.git',
+      path: '~/movies/backend',
+      'post-deploy': 'npm install',
+    },
+  },
+};
